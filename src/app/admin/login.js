42e00b6abf6f2f4270fede3a40c12b2d267e7f6d
Naveen Vangalapudi <naveen@deepgrid.in>
2022-12-09 17:07:36 +0530

    basic setup

---
+++ src/app/admin/login.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+class Login extends React.Component {
+
+    constructor(props){
+        super(props);
+
+    }
+//life cycles
+    componentDidMount(){
+
+    }
+
+
+    render(){
+// declare variables and fucntions in render 
+
+
+
+
+
+        return(
+            //write html code in return 
+            <div>Login Screen Component</div>
+        )
+    }
+}
+
+export default Login;
